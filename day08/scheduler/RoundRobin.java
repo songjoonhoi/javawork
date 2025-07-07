074ce81d61601b83ee167352ac55b8784d934a75
SongJoonHoi <wnsghl9897@naver.com>
2025-07-07 23:33:29 +0900

    인퍼테이스 공부

---
+++ day08/scheduler/RoundRobin.java
@@ -0,0 +1,19 @@
+package day08.scheduler;
+
+public class RoundRobin implements Scheduler{
+
+    @Override
+    public void getNextCall() {
+        
+       System.out.println("상담 전화를 순서대로 대기열에서 가져옵니다.");
+    }
+
+    @Override
+    public void sendCallToAgent() {
+        System.out.println("다음 순서 상담원에세 배분합니다.");
+      
+    }
+
+    
+    
+}
